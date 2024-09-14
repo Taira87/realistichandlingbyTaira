a0d40c0a4e2a1b5669909403d6b27611480aca16
Jose Caballero <90627260+Taira87@users.noreply.github.com>
2024-09-14 16:56:09 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,17 +7,17 @@ author 'Taira87'
 description 'coches vanilla'
 
 files {
-    'stream/****/vehiclelayouts.meta',
+    -- 'stream/****/vehiclelayouts.meta',
     'stream/****/handling.meta',
-    'stream/****/vehicles.meta',
-    'stream/****/carcols.meta',
-    'stream/****/carvariations.meta',
-    'stream/****/contentunlocks.meta',
+    -- 'stream/****/vehicles.meta',
+    -- 'stream/****/carcols.meta',
+    -- 'stream/****/carvariations.meta',
+    -- 'stream/****/contentunlocks.meta',
 }
 
-data_file 'VEHICLE_LAYOUTS_FILE' 'stream/****/vehiclelayouts.meta'
+-- data_file 'VEHICLE_LAYOUTS_FILE' 'stream/****/vehiclelayouts.meta'
 data_file 'HANDLING_FILE' 'stream/****/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'stream/****/vehicles.meta'
-data_file 'CARCOLS_FILE' 'stream/****/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'stream/****/carvariations.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'stream/****/contentunlocks.meta'
+-- data_file 'VEHICLE_METADATA_FILE' 'stream/****/vehicles.meta'
+-- data_file 'CARCOLS_FILE' 'stream/****/carcols.meta'
+-- data_file 'VEHICLE_VARIATION_FILE' 'stream/****/carvariations.meta'
+-- data_file 'CONTENT_UNLOCKING_META_FILE' 'stream/****/contentunlocks.meta'
